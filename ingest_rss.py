388d2db6057d3fc3bd4920f59c0b3d8b73cfc689
ArasuBS <arasuatcalicut@gmail.com>
2026-07-29 17:54:39 +0530

    Update ingest_rss.py

--- ingest_rss.py
+++ ingest_rss.py
@@ -1,1 +1,12 @@
+import feedparser
+
+RSS_URL = "https://www.fiercepharma.com/rss/xml"
+
 print("RSS script started")
+
+feed = feedparser.parse(RSS_URL)
+
+print(f"Found {len(feed.entries)} articles")
+
+for article in feed.entries[:5]:
+    print(article.title)
