73e4ecac1bb6ff89adf31db733c0eb8750f1964b
ArasuBS <arasuatcalicut@gmail.com>
2026-07-30 10:19:11 +0530

    Update classify_records.py

--- classify_records.py
+++ classify_records.py
@@ -1,1 +1,12 @@
+import json
+
 print("Classifier started")
+
+with open("data/pubmed_results.json", "r", encoding="utf-8") as f:
+    pubmed_records = json.load(f)
+
+with open("data/rss_results.json", "r", encoding="utf-8") as f:
+    rss_records = json.load(f)
+
+print(f"PubMed records: {len(pubmed_records)}")
+print(f"RSS records: {len(rss_records)}")
